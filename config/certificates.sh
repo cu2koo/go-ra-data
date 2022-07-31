c3da64556bda25f9531e83d61a4d14fb261eac98
Sercan Yesildal <sercan.yesildal@gmail.com>
2022-07-31 19:24:04 +0200

    source added

--- config/certificates.sh
+++ config/certificates.sh
@@ -1,3 +1,4 @@
+# Based on the commands from Köhler https://github.com/SourceFellows/gobuch/tree/master/microservices/httpsserver
 openssl genrsa -out root.key 2048
 openssl req -x509 -new -nodes -key root.key -sha256 -days 365 -out root.crt
 openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key
